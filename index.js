85bc68f614033c5c793a24065c1221c00e836545
DJ Versoza <djversoza@gmail.com>
2018-10-18 11:42:18 -0700

    updating package

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ exports.testQueue = function() {
   let rsmq = new RedisSMQ();
   rsmq.createQueue({qname:"myqueue"}, function (err, resp) {
 		if (resp===1) {
-			console.log("queue created")
+      console.log("queue created")
+      rsmq.listQueues( function (error, queues) {
+        if( error ){
+          console.error( error )
+          return
+        }
+        console.log("Active queues: " + queues.join( "," ) )
+      });
 		}
 });
   }
